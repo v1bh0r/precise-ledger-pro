5c661d187811ea53c034d147798f8a85b53f8e72
Vibhor Mahajan <vmahajan@lendingpoint.com>
2024-05-20 11:57:28 +0530

    precise-ledger-pro-9 Develop the demo APIs and added some persistence
    Adding a postgres

--- src/main/java/ledger/api/loan/LoanCreationRequest.java
+++ src/main/java/ledger/api/loan/LoanCreationRequest.java
@@ -2,9 +2,12 @@ package ledger.api.loan;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class LoanCreationRequest {
     private Integer daysInYear;
     private String currencyCode;
     private String externalId;
+    private LocalDateTime lastLedgerFrozenOn = LocalDateTime.MIN;
 }
