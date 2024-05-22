9802d43058b926e2ed0b1b522dcbf9aaeafffecc
Vibhor Mahajan <vmahajan@lendingpoint.com>
2024-05-22 16:08:46 +0530

    precise-ledger-pro-19 Exposed attributes related to initial balance when creating a loan

--- src/main/java/ledger/api/loan/LoanCreationRequest.java
+++ src/main/java/ledger/api/loan/LoanCreationRequest.java
@@ -4,12 +4,18 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+import static ledger.config.AppConfig.DEFAULT_CURRENCY_CODE;
+import static ledger.config.AppConfig.DEFAULT_DAYS_IN_YEAR;
 import static ledger.util.DateTimeUtil.DB_SAFE_LOCAL_DATETIME_MIN;
 
 @Data
 public class LoanCreationRequest {
-    private Integer daysInYear;
-    private String currencyCode;
+    private Integer daysInYear = DEFAULT_DAYS_IN_YEAR;
+    private String currencyCode = DEFAULT_CURRENCY_CODE;
     private String externalId;
+    private Double lastLedgerFreezePrincipalBalance = 0.0;
+    private Double lastLedgerFreezeInterestBalance = 0.0;
+    private Double lastLedgerFreezeFeeBalance = 0.0;
+    private Double lastLedgerFreezeExcessBalance = 0.0;
     private LocalDateTime lastLedgerFrozenOn = DB_SAFE_LOCAL_DATETIME_MIN;
 }
