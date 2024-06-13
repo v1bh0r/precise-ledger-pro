0c883d267554779061acfcea17355a49dd6257f1
Vibhor Mahajan <vmahajan@lendingpoint.com>
2024-06-13 12:43:12 +0530

    Fixes

--- src/main/webui/src/main.js
+++ src/main/webui/src/main.js
@@ -3,15 +3,11 @@ import uk from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
 uk.use(Icons)
 
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store' // Import the store
 
-
 const app = createApp(App)
 
-app.use(router)
-  .use(store)
-  .mount('#app')
+app.use(router).use(store).mount('#app')
